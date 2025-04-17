4e1024b67a2138261ee837166621a9465dba01fd
Sindu-rgh <sindu.k@s.amity.edu>
2025-04-17 20:37:12 +0530

    part-1 model

--- train_model.py
+++ train_model.py
@@ -2,8 +2,8 @@ def train_model():
     train_data, val_daata,test_data=
     load_data(train_DIR, VAL_DIR, TEST_DIR)
     model=
-
-
-
+create_model(input_shape=(IMG_SIZE[0],
+                          IMG_SIZE[1], 3),
+             num_classes=train_data.num_classes)
 
 
