06266565bf8370dbfc01b7e033b8f7305b26aa4d
Sindu-rgh <sindu.k@s.amity.edu>
2025-04-17 20:32:24 +0530

    part_1

--- train_model.py
+++ train_model.py
@@ -2,8 +2,8 @@ def train_model():
     train_data, val_daata,test_data=
     load_data(train_DIR, VAL_DIR, TEST_DIR)
     model=
-    
-             label='validation accuracy')
-    plt.xlabel('Epoch')
-    plt.ylabel('accuracy')
-    plt.legend()
+
+
+
+
+
