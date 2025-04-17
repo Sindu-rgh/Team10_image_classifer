ae2d327cd40bb05ac84f3c8852cdd0143710614c
Sindu-rgh <sindu.k@s.amity.edu>
2025-04-17 21:04:02 +0530

    simple 4

--- train_model.py
+++ train_model.py
@@ -31,3 +31,8 @@ print("training complete.model
 #======== 4. PREDICT SINGLE IMAGE
 ========
 def predict_image(img_path):
+    model=load_model(MODEL_PATH)
+    img=load_img(img_path,
+                 targeted_size=IMG_SIZE)
+    img_array=img_to_array(img)/
+    255.0
