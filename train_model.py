bb26b66ff33ff054e807dc7a7b016dce6793d41c
Sindu-rgh <sindu.k@s.amity.edu>
2025-05-16 10:34:53 +0530

    added another last line

--- train_model.py
+++ train_model.py
@@ -3,4 +3,5 @@ train_data, val_data,test_data=
 load_data(train_DIR,VAL_DIR, TEST_DIR)
 model=
 create_model1(input_shape=(IMG_SIZE[0],
-                         IMG_SIZE[1],3),  
+                         IMG_SIZE[1],3),
+              num_classes=train_data.num_classes)
