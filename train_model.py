4548c7932be4bbffdc9ee42b4f42693f81180a30
Sindu-rgh <sindu.k@s.amity.edu>
2025-05-18 18:09:44 +0530

    converting

--- train_model.py
+++ train_model.py
@@ -1,7 +1,6 @@
 <<<<<<< HEAD
-
 callbacks=[
-       modelcheckpoint(MODEL_PATH,
+modelcheckpoint(MODEL_PATH,
                        save_best_only=true),
        Earlystopping(patience=5,
                      restore_best_weight=true)
